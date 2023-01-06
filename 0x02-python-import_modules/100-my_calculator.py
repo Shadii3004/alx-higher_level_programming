d541bb1c4ad6246fb84971adc839c0ad9c47ba67
Shadii3004 <shadrachmohzo@gmail.com>
2023-01-05 19:07:07 -0800

    My 100 task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(ops.keys()):
+
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if (__name__ == "__main__"):
+    argc = len(argv)
+    result = 0
+    operators = ["+", "-", "*", "/"]
+    if (argc != 4):
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+        if (argv[2] not in operators):
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+            exit(1)
+            num1 = int(argv[1])
+            operator = argv[2]
+            num2 = int(argv[3])
+            if (operator == "+"):
+                result = add(num1, num2)
+            elif (operator == "-"):
+                result = sub(num1, num2)
+            elif (operator == "*"):
+                result = mul(num1, num2)
+            else:
+                result = div(num1, num2)
+                print(f"{num1} {operator} {num2} = {result}")
